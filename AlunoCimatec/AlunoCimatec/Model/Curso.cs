a43a091f8c8b5af86eafca5ae438a405957289c5
Alex <alexvingg@gmail.com>
2016-03-14 19:59:09 -0300

    comit incial

---
+++ AlunoCimatec/AlunoCimatec/Model/Curso.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlunoCimatec.Model
+{
+    public class Curso
+    {
+        public String Nome { get; set; }
+
+        public List<Disciplinas> Disciplinas { get; set; }
+
+        public String InicialCurso
+        {
+            get
+            {
+                return  this.Nome.Substring(this.Nome.Length - 1, 1);
+            }
+            set
+            {
+                this.InicialCurso = value;
+            }
+        }
+
+        //public String getInicalCurso()
+        //{
+        //    return this.Nome.Substring(1, this.Nome.Length);
+        //}
+    }
+}
---
+++ AlunoCimatec/AlunoCimatec/Model/Disciplinas.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlunoCimatec.Model
+{
+    public class Disciplinas
+    {
+        public String Nome { get; set; }
+
+        public String Professor { get; set; }
+    }
+}
